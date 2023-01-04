b4e110fbe80a4822b861853894bbeb88b7821dd7
Vladkrivonossov <vladik374374@gmail.com>
2023-01-04 17:58:27 +0300

    fix: run lint

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,15 +1,24 @@
-import React, {FC, ReactNode} from 'react';
-import './Button.css'
+import React, { FC, ReactNode } from 'react';
+import './Button.css';
 
 export type ButtonProps = {
-    onClick(): void;
-    children: ReactNode;
-    variant?: 'primary' | 'success';
-    isDisabled?: boolean;
-}
+  onClick(): void;
+  children: ReactNode;
+  variant?: 'primary' | 'success';
+  isDisabled?: boolean;
+};
 
-export const Button: FC<ButtonProps> = ({ onClick, variant = "primary", isDisabled = false, children }) => {
-    const className = `button button-${variant}`
+export const Button: FC<ButtonProps> = ({
+  onClick,
+  variant = 'primary',
+  isDisabled = false,
+  children,
+}) => {
+  const className = `button button-${variant}`;
 
-    return <button onClick={onClick} className={className} disabled={isDisabled}>{children}</button>
-}
+  return (
+    <button onClick={onClick} className={className} disabled={isDisabled}>
+      {children}
+    </button>
+  );
+};
